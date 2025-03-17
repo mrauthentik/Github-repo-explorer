fcb58c2d8cd9a29afdd4a971322f95b090194e80
mrauthentik <umokeuchenna2020@gmail.com>
2025-03-17 13:34:32 +0100

    refactor App component to use TypeScript and improve structure

--- git-repo-explorer/src/App.tsx
+++ git-repo-explorer/src/App.tsx
@@ -1,10 +1,19 @@
+import React from "react"
+import {}
 
-import './App.css'
+const POSTS = [
+  {id: 1, title: "Post 1"},
+  {id: 2, title: "Post 2"},
 
-export default function App(){
+]
+
+ const App: React.FC = ()=>{
   return(
-    <div className='bg-blue-500'>
-      <h1 className='text-white text-center'>TanStack Query</h1>
-    </div>
+    <h1 className="bg-blue-500 text-center text-white"> TanStack </h1>
   )
 }
+
+function wait (duration:number){
+      return new Promise(resolve => setTimeout(resolve, duration))
+}
+export default App
