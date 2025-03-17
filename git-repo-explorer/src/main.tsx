34b48fb1a30597e6954ff5d47959c8cb3504139e
mrauthentik <umokeuchenna2020@gmail.com>
2025-03-17 13:07:51 +0100

    refactor main.tsx to rename QueryClient instance for clarity

--- git-repo-explorer/src/main.tsx
+++ git-repo-explorer/src/main.tsx
@@ -4,12 +4,14 @@ import './index.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import App from './App.tsx'
 
-const ucheClient = new QueryClient();
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QueryClientProvider client={ucheClient}>
+   
+    <QueryClientProvider client={queryClient} >
      <App /> 
     </QueryClientProvider>
+    
   </StrictMode>,
 )
