5bb43669eff6a44b6626873032c4c73e95b9d0d4
mrauthentik <umokeuchenna2020@gmail.com>
2025-03-17 13:06:23 +0100

    add undici types and update package.json for TypeScript support

--- git-repo-explorer/src/main.tsx
+++ git-repo-explorer/src/main.tsx
@@ -4,11 +4,11 @@ import './index.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import App from './App.tsx'
 
-const queryClient = new QueryClient();
+const ucheClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
+    <QueryClientProvider client={ucheClient}>
      <App /> 
     </QueryClientProvider>
   </StrictMode>,
