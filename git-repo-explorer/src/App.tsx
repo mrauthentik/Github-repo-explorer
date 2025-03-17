18a23139f8aea06ec54b5ed303499d2edfbc74f3
mrauthentik <umokeuchenna2020@gmail.com>
2025-03-17 14:14:08 +0100

    feat: add Post interface and implement newPost mutation for dynamic post creation

--- git-repo-explorer/src/App.tsx
+++ git-repo-explorer/src/App.tsx
@@ -1,6 +1,11 @@
 import React from "react"
 import {useQuery, useMutation} from '@tanstack/react-query'
- 
+
+interface Post {
+  id: string;
+  title: string;
+}
+
 const POSTS = [
   {id: 1, title: "Post 1"},
   {id: 2, title: "Post 2"},
@@ -14,7 +19,13 @@ const POSTS = [
     queryFn: ()=> wait(1000).then(()=> [...POSTS])
   })
 
-
+  const newPost = useMutation({
+    mutationFn:({title}:{title:string}) => {
+      return (
+        wait(1000).then(()=> POSTS.push({id: crypto.randomUUID(), title}))
+      )
+    }
+  })
 
 if(allPostQuery.isLoading) return <h1 className="text-green-600 text-xl p-5"> Loading ....</h1>
 if(allPostQuery.isError) return <h2 className="text-red-700">Error: {JSON.stringify(allPostQuery.error)}</h2>
