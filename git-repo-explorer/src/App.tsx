efdbaa41baf83937322e5b0239a70deb4218d306
mrauthentik <umokeuchenna2020@gmail.com>
2025-03-17 13:49:21 +0100

    refactor App component to improve post rendering and remove unused imports

--- git-repo-explorer/src/App.tsx
+++ git-repo-explorer/src/App.tsx
@@ -1,7 +1,6 @@
 import React from "react"
 import {useQuery, useMutation} from '@tanstack/react-query'
-import { error } from "console"
-
+ 
 const POSTS = [
   {id: 1, title: "Post 1"},
   {id: 2, title: "Post 2"},
@@ -15,10 +14,20 @@ const POSTS = [
     queryFn: ()=> wait(1000).then(()=> [...POSTS])
   })
 
+
+
 if(allPostQuery.isLoading) return <h1 className="text-green-600 text-xl p-5"> Loading ....</h1>
 if(allPostQuery.isError) return <h2 className="text-red-700">Error: {JSON.stringify(allPostQuery.error)}</h2>
   return(
-    <h1 className="bg-blue-500 text-center text-white"> TanStack </h1>
+    <div>
+
+      <h1 className="bg-blue-500 text-center text-white"> TanStack </h1>
+      {
+        allPostQuery.data?.map((item)=> {
+          <div>{item.title} {item.id}</div>
+        })
+      }
+    </div>
   )
 }
 
