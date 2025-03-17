f22884d41fafc239eb9cb08827243cda53094a77
mrauthentik <umokeuchenna2020@gmail.com>
2025-03-17 13:19:12 +0100

    remove unused files and dependencies from node_modules

--- git-repo-explorer/src/App.tsx
+++ git-repo-explorer/src/App.tsx
@@ -3,8 +3,8 @@ import './App.css'
 
 export default function App(){
   return(
-    <div>
-      TanStack Query
+    <div className='bg-blue-500'>
+      <h1 className='text-white'>TanStack Query</h1>
     </div>
   )
 }
