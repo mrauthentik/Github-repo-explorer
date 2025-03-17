3f766729ecc65e391fa2e4b74fd2ec6e5e3ea9bd
mrauthentik <umokeuchenna2020@gmail.com>
2025-03-17 13:22:18 +0100

    enhance App component styling and integrate Tailwind CSS with Vite

--- git-repo-explorer/src/App.tsx
+++ git-repo-explorer/src/App.tsx
@@ -4,7 +4,7 @@ import './App.css'
 export default function App(){
   return(
     <div className='bg-blue-500'>
-      <h1 className='text-white'>TanStack Query</h1>
+      <h1 className='text-white text-center'>TanStack Query</h1>
     </div>
   )
 }
