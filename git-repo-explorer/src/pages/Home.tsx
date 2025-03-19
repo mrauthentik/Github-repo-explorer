261656b185c7c94cc28487659b8e6377b1126b71
mrauthentik <umokeuchenna2020@gmail.com>
2025-03-19 10:08:42 +0100

    feat: initialize Home component with default query and integrate SearchBar and RepoList

--- git-repo-explorer/src/pages/Home.tsx
+++ git-repo-explorer/src/pages/Home.tsx
@@ -2,3 +2,17 @@ import React, { useState } from "react";
 import SearchBar from "../components/SearchBar";
 import RepoList from "../components/RepoList";
 
+
+const Home: React.FC = () => {
+    const [query, setQuery] = useState("react");
+  
+    return (
+      <div className="max-w-2xl mx-auto p-4">
+        <h1 className="text-xl font-bold text-center">GitHub Repo Explorer</h1>
+        <SearchBar onSearch={setQuery} />
+        <RepoList query={query} />
+      </div>
+    );
+  };
+  
+  export default Home;
