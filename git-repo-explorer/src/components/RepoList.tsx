21c8c253a7527a7335542a995f65dfd879952677
mrauthentik <umokeuchenna2020@gmail.com>
2025-03-19 10:09:53 +0100

    fix: update import path for fetchRepos in RepoList component

--- git-repo-explorer/src/components/RepoList.tsx
+++ git-repo-explorer/src/components/RepoList.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import {useQuery} from '@tanstack/react-query'
-import { fetchRepos } from "@/api/githubApi"
+import { fetchRepos } from "../api/githubApi"
 
 interface RepoListProps {
     query: string
