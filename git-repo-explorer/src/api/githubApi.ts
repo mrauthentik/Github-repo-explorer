acb65ca1107ed5510204fa914bf047a649979691
mrauthentik <umokeuchenna2020@gmail.com>
2025-03-19 10:41:27 +0100

    feat: implement logic to fetch latest commit details for each repository

--- git-repo-explorer/src/api/githubApi.ts
+++ git-repo-explorer/src/api/githubApi.ts
@@ -5,5 +5,20 @@ export const fetchRepos = async (query: string) =>{
         throw new Error('Failed to fetch repositories')
         console.log('Failed to fetch repos')
     }
+
+    const data = await response.json()
+    const repos = data.items || []
+
+    //This Logic Fetches commit details for each repo
+    const reposWithCommits = await Promise.all(
+        repos.map(async (repo: any)=> {
+            try{
+                const commitResponse = await fetch (
+                    `https://api.github.com/repos/${repo.owner.login}/${repo.name}/commits?per_page=1`
+                );
+            }
+        })
+    )
+
     return response.json()
 }
